fe1569d24383378a8b8aee2d2003d9817e3dc047
Ross Wu <rosswu@rosswu-macbookpro3.roam.corp.google.com>
2020-03-23 10:07:10 +0800

    Add my zsh confs.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Ross' Linux Environment
+
+
+## 1. Zsh
+
+### 1.1 Install Oh-My-Zsh
+
+Follow the [**Manual Installation**](https://github.com/ohmyzsh/ohmyzsh#manual-installation)
+instructions at the Oh-My-Zsh Github repo.
+
+### 1.2 Setup Oh-My-Zsh with my configs.
+
+1.  Clone my linux repo.
+
+```
+git clone https://github.com/ross-wu/linux.git $HOME/linux
+```
+
+2.  Setup.
+
+```
+cd $HOME/linux
+./setup-zsh.sh
+```
+
---
+++ setup-zsh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+repo_dir=$(cd `dirname $0` && pwd)
+
+echo "repo_dir=${repo_dir}"
+
+cd $HOME
+
+bak_dir=$HOME/bak/linux_`date "+%Y%m%d-%H.%M.%S"`
+mkdir -p $bak_dir
+
+mv .zshrc $bak_dir/bak.zshrc
+ln -s ${repo_dir}/_zshrc ${HOME}/.zshrc
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: creating my .zshrc"
+    exit 255
+fi
+
+mv ${HOME}/.oh-my-zsh/custom/themes/ross.zsh-theme $bak_dir/
+ln -s ${repo_dir}/ross.zsh-theme ${HOME}/.oh-my-zsh/custom/themes/ross.zsh-theme
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: creating ross.zsh-theme"
+    exit 255
+fi
+
